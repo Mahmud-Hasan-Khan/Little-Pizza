3bc15b41c0cc6fe7ab63227662f85be7e2c8e77a
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2026-01-04 17:32:06 +0600

    hamburger SVG added in navber

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -81,8 +81,10 @@ export default function Navbar() {
               </svg>
             </Link>
             <button onClick={() => setIsOpen(!isOpen)} className="text-gray-800">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+              <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4 6H20" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M4 12H20" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M4 18H20" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </button>
           </div>
